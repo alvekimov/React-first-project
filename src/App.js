8cf0d36890e4de77762fee43b61e722a7dd7000b
alvekimov <al.v.ekimov@gmail.com>
2021-11-02 21:27:58 +0500

    19. Настроили route для NavBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
-// import Profile from './components/Profile/Profile';
+import Profile from './components/Profile/Profile';
 import './App.css';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <NavBar/>
-            <div className="app-wrapper-content">
-                {/*<Profile />*/}
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <NavBar/>
+                <div className="app-wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 

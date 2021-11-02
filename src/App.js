3e0670be1c77eaba32301317cbf4572984fac65c
alvekimov <al.v.ekimov@gmail.com>
2021-11-02 20:28:08 +0500

    18.Добавили компоненту Dialogs, перешли на webstorm, отредактировали gitignore

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
-import Profile from './components/Profile/Profile';
+// import Profile from './components/Profile/Profile';
 import './App.css';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <NavBar />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <NavBar/>
+            <div className="app-wrapper-content">
+                {/*<Profile />*/}
+                <Dialogs/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,22 +3,22 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css'
 
 const Profile = () => {
-  return (
-    <main className={s.content}>
-      <div className={s.backImg}>
-        <img src="https://sebweo.com/images/landshaft-bernskikh-alp-v-yasniy-den.jpg" alt="nature" />
-      </div>
-      <div className={s.avatarDescription}>
-        <div className={s.avatar}>
-          avatar
-        </div>
-        <div className={s.description}>
-          description
-        </div>
-      </div>
-      <MyPosts />
-    </main>
-  );
+    return (
+        <main className={s.content}>
+            <div className={s.backImg}>
+                <img src="https://sebweo.com/images/landshaft-bernskikh-alp-v-yasniy-den.jpg" alt="nature"/>
+            </div>
+            <div className={s.avatarDescription}>
+                <div className={s.avatar}>
+                    avatar
+                </div>
+                <div className={s.description}>
+                    description
+                </div>
+            </div>
+            <MyPosts/>
+        </main>
+    );
 }
 
 export default Profile;

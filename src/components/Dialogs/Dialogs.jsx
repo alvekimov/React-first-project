f4c7d23058cd1e00ec24dc57180c6257f47fbfb3
alvekimov <al.v.ekimov@gmail.com>
2021-11-02 23:02:35 +0500

    21. верстка страницы диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,22 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Alex</div>
+                <div className={`${s.dialog} ${s.active}`}>Dima</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Kate</div>
+                <div className={s.dialog}>Misha</div>
+                <div className={s.dialog}>Petr</div>
+                <div className={s.dialog}>Viktor</div>
+                <div className={s.dialog}>Andrey</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>yo</div>
+            </div>
         </div>
     );
 }
